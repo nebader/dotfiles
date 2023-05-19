8a2cc9961d7024600e14a31adb0e457fab0de2df
nebader <nbader@gmail.com>
2023-05-19 14:34:26 -0700

    changes to emacs: completion framework to vertico, minor UI changes

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,27 +1,33 @@
 ;; This init.el file is run by emacs when it first starts loading.
 ;;   This will eventually be a minimal configuration designed to set up package
 ;;   managers, enable org mode, then run emacs-config.org, where the
-;;   rest of the configuration lives.
+;;   rest of the configuration will live.
 ;; Nick Bader
-;; Last updated: June 2022
+;; Last updated: May 2023
+;; System Crafters 1: UI, package manager, completion framework
 
-;; -- Package management part 1: package.el --
+;; -- Package management --
 
-; Load local file "package.el" to start built-in package manager
+; Load local file "package.el" to enable package mgmt functions
 (require 'package)
 
 ; Change package.el 'archives' variable to add additional repositories
 (setq package-archives '(
-	("gnu" . "https://elpa.gnu.org/packages/")
-	("melpa" . "https://melpa.org/packages/")
-        ))
+			 ("melpa" . "https://melpa.org/packages/")			 
+			 ("gnu" . "https://elpa.gnu.org/packages/")
+			 ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+			 ))
 
 ; Tell package.el to load packages in its load list
 (package-initialize)
 
 ; Only update list of available packages if the list doesn't exist (new install)
 (unless package-archive-contents ; evals to TRUE if list exists
-  package-refresh-contents)      ; call this fxn to update manually
+  package-refresh-contents)
+; Current problem: if you installed emacs a while ago, then try to install a package,
+; use-package will "fail to install" because the list is outdated.  You can run
+; M-x package-refresh-contents manually if you see this error message.
+; In the long term, moving to straght.el might be the way to fix this.
 
 ; Bootstrap use-package, a package that changes the way that packages
 ; are specified and loaded, and enables lazy loading
@@ -29,21 +35,63 @@
   (package-refresh-contents)               ; call this fxn to update manually
   (package-install 'use-package))          ; install if missing
 (require 'use-package)                     ; load use-package
-;(setq use-package-always-ensure t)         ; installs missing pkgs (or do in each use-package)
+(setq use-package-always-ensure t)         ; installs missing pkgs (or do in each use-package)
 
+; -- Completion framework --
+
+; IDO mode
+;; (setq ido-enable-flex-matching t)
+;; (setq ido-everywhere t)
+;; (ido-mode 1)
+
+; vertico displays candidates in a vertical list in the minibuffer
+(use-package vertico
+  :ensure t
+  :custom
+  (vertico-cycle t)
+  :init
+  (vertico-mode)
+  )
+
+; savehist ("saved history") is a built-in package that puts your recently-selected files at the top of the list.
+;; (use-package savehist
+;;   :init
+;;   (savehist-mode)
+;;   )
+
+; orderless lets you enter space separated search terms for filtering
+(use-package orderless
+  :ensure t
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
+
+;; Marginalia displays annotations in the minibuffer
+(use-package marginalia
+  ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
+  ;; available in the *Completions* buffer, add it to the
+  ;; `completion-list-mode-map'.
+  :bind (:map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+  ;; The :init section is always executed.
+  :init
+  ;; Marginalia must be actived in the :init section of use-package such that
+  ;; the mode gets enabled right away. Note that this forces loading the
+  ;; package
+  (marginalia-mode))
+
+; ----------------------------- ;
+
+
+; -- shell stuff?? --
 ; This package attempts to set emacs exec-path and PATH from the shell path
-; Should work if your shell is found by (getenv "SHELL")
+; Should work if your shell is found by executing (getenv "SHELL")
 (use-package exec-path-from-shell
-  :ensure t ; install if not installed already
   :if (memq window-system '(mac ns x)) ; when window mgr is either mac, ns, or x
   :config
   (exec-path-from-shell-initialize)
   )
 
-; Turn on IDO mode
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
 
 ;; ; Use Evil mode (vim emulator)
 ;; ;; load evil
@@ -81,13 +129,11 @@
 
 ; git interface
 (use-package magit
-  :ensure t
   :bind (("C-x g" . magit))
   )
 
 ; R interface
-(use-package ess
-  :ensure t)
+(use-package ess)
 
 ; LaTeX interface
 (use-package latex
@@ -97,28 +143,32 @@
   )
 
 ; Editable inline LaTeX snippets in org
-(use-package org-fragtog
-  :ensure t
-  )
+(use-package org-fragtog)
+
+
+; ------------- Emacs UI ----------------
 
 
 ;; Simplify the Emacs UI
-(setq inhibit-startup-screen t) ; same as startup-message/splash-screen
-(tool-bar-mode -1)              ; turn off the graphics buttons
+(setq inhibit-startup-screen t) ; "startup-screen" is same as "startup-message" and "splash-screen"
+(tool-bar-mode -1)              ; turn off the graphical buttons
 (menu-bar-mode -1)              ; turn off the menu bar at the top
 (scroll-bar-mode -1)            ; turn off the right-hand scrollbar
 (tooltip-mode -1)               ; don't use tooltips on mouseover
-
+(set-fringe-mode 10)            ; (default is 8 pix) adds a little room on the right and left
 
 ;; Appearance
 (set-face-attribute 'default nil :font "inconsolata" :height 200) ; font
 (load-theme 'misterioso) ; theme
 (add-to-list 'initial-frame-alist '(fullscreen . maximized)) ; start in big window
 
+(setq help-at-pt-display-when-idle t) ; shows tooltips in minibuffer
+
+
 ; Mode line changes
-;(use-package doom-modeline
-;  :ensure t
-;  :init (doom-modeline-mode 1))
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
 
 
 ;; - Housecleaning -
@@ -147,6 +197,10 @@
 ;        mac-right-option-modifier 'nil
 ;	mac-command-modifier 'nil
 ;	mac-right-command-modifier 'meta))
+
+; On Mac, command-h bypasses emacs to hide the window.  Prevent emacs from sending this
+; command-tab still works as macos app switcher!
+(setq mac-pass-command-to-system nil)
 
 ;; - Interaction with system clipboard
 (setq-default
